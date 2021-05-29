6c998f1265f94f639aae6d47584b9bd96d6d5b2c
Kim Seohyun <kshjessica@ewhain.net>
2021-05-29 13:42:01 +0900

    fix story API

---
+++ src/api/story.ts
@@ -0,0 +1,32 @@
+import express, { Request, Response } from "express";
+import { Mongoose } from "mongoose";
+const router = express.Router();
+
+import Story from "../models/Story";
+
+router.get("/", async(req: Request, res: Response) => {
+        try{
+            const randomInt = Math.floor(Math.random()*2);
+            const story = await Story.findOne({storyIdx : randomInt});
+            res.json({
+                "status" : 200,
+                "success" : true,
+                "message" : "성공",
+                "data" : {
+                    img: story.img,
+                    brand: story.brand,
+                    view: story.view,
+                    }
+                })
+            
+        } catch(error) {
+            console.error(error.message);
+            res.status(500).json({
+                "status" : 500,
+                "success" : false,
+                "message" : "Server Error"
+            });
+        }
+});
+
+module.exports = router;
