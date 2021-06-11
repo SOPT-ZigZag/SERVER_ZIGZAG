2094776f226a18db4402fa991fadf65aa369ffe4
Kim Seohyun <kshjessica@ewhain.net>
2021-06-11 20:57:12 +0900

    update store API(using list)

--- src/api/store.ts
+++ src/api/store.ts
@@ -6,21 +6,27 @@ import Store from "../models/Store";
 
 router.get("/", async(req: Request, res: Response) => {
         try{
-            const randomInt = Math.floor(Math.random()*3);
-            const store = await Store.findOne({store_idx : randomInt});
+            var store =  await Store.find({store_idx : 1})
+  
+            store = store.sort(function() {
+                    return Math.random() - Math.random();
+                    });
+            // const randomInt = Math.floor(Math.random()*3);
+            // const store = await Store.findOne({store_idx : randomInt});
             res.json({
                 "status" : 200,
                 "success" : true,
-                "message" : "성공",
+                "message" : "쇼핑몰 API 불러오기 성공",
                 "data" : {
-                    img: store.img,
-                    store_idx: store.store_idx,
-                    brand: store.brand,
-                    style: store.style,
-                    coupon: store.coupon,
-                    bookmark: store.bookmark,
-                    z_only: store.z_only,
-                    follower: store.follower,
+                    "store" : store
+                    // img: store.img,
+                    // store_idx: store.store_idx,
+                    // brand: store.brand,
+                    // style: store.style,
+                    // coupon: store.coupon,
+                    // bookmark: store.bookmark,
+                    // z_only: store.z_only,
+                    // follower: store.follower,
                     }
                 })
             
