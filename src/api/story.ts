1ca2c6ac19fa28ff3c0bd693e3be138e448cae8c
Kim Seohyun <kshjessica@ewhain.net>
2021-06-11 20:59:57 +0900

    update story API(using list)

--- src/api/story.ts
+++ src/api/story.ts
@@ -6,17 +6,23 @@ import Story from "../models/Story";
 
 router.get("/", async(req: Request, res: Response) => {
         try{
-            const randomInt = Math.floor(Math.random()*3);
-            const story = await Story.findOne({story_idx : randomInt});
+            var story =  await Story.find({story_idx : 1})
+  
+            story = story.sort(function() {
+                    return Math.random() - Math.random();
+                    });
+            // const randomInt = Math.floor(Math.random()*3);
+            // const story = await Story.findOne({story_idx : randomInt});
             res.json({
                 "status" : 200,
                 "success" : true,
-                "message" : "성공",
+                "message" : "스토어 API 불러오기 성공",
                 "data" : {
-                    img: story.img,
-                    story_idx: story.story_idx,
-                    brand: story.brand,
-                    view: story.view,
+                    "story" : story
+                    // img: story.img,
+                    // story_idx: story.story_idx,
+                    // brand: story.brand,
+                    // view: story.view,
                     }
                 })
             
